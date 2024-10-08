7d1fdd666865936be161163e9913bee4eb219363
Maryam Mirafzal/ <maryammirafzal9@gmail.com>
2024-10-08 17:32:13 +0330

    [ADD] Complete BlogPage Details
    [+] Add Comment Form

--- src/components/blog/BlogPage.jsx
+++ src/components/blog/BlogPage.jsx
@@ -1,10 +1,15 @@
 import { useQuery } from "@apollo/client"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { Get_POST_INFO } from "../../graphQL/queries"
 import Loader from "../shared/Loader"
+import { Avatar, Box, Container, Grid, Typography } from "@mui/material"
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import sanitizeHtml from "sanitize-html"
+import CommentForm from "../comment/CommentForm"
 
 function BlogPage() {
   const { slug } = useParams()
+  const navigate = useNavigate()
 
   const { loading , data , errors } = useQuery(Get_POST_INFO, {
     variables:{ slug },
@@ -15,7 +20,37 @@ function BlogPage() {
   if(errors) return <h3>Error...</h3>
   console.log(data);
   return (
-    <div>BlogPage</div>
+    <Container maxWidth='lg'>
+      <Grid container>
+        <Grid item xs={12} mt={9} display='flex' justifyContent='space-between'>
+          <Typography component="h2" variant="h4" color="primary" fontWeight={700}>
+            {data.post.title}
+          </Typography>
+          <ArrowBackIcon onClick={()=> navigate(-1)} />
+        </Grid>
+        <Grid item xs={12} mt={6}>
+          <img src={data.post.coverPhoto.url} alt={data.post.slug} width='100%'
+          style={{ borderRadius: 15}}/>
+        </Grid>
+        <Grid item xs={12} mt={7}>
+          <Avatar src={data.post.author.avatar.url} sx={{width: 80 , height: 80, marginLeft: 2}}/>
+        </Grid>
+        <Box component='div'>
+          <Typography component='p' variant="h5" fontWeight={700}>
+            {data.post.author.name}
+          </Typography>
+          <Typography component='p' variant="p" color="text.secondary">
+            {data.post.author.feild}
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={12} mt={5}>
+        <div dangerouslySetInnerHTML={{__html: sanitizeHtml(data.post.content.html)}}></div>
+      </Grid>
+      <Grid item xs={12} >
+        <CommentForm slug={slug} />
+      </Grid>
+    </Container>
   )
 }
 
